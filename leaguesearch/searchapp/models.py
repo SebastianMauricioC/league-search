333e151d01215d674b027bc70b79898bf151b457
SebastianMauricioC <sebastiancabrera1111@gmail.com>
2024-01-27 19:40:41 -0300

    Champions

--- leaguesearch/searchapp/models.py
+++ leaguesearch/searchapp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Summoner(models.Model):
+    summoner_puuid = models.CharField(max_length=78)
+    summoner_name = models.CharField(max_length=30)
+    summoner_region = models.CharField(max_length=5)
+    summoner_level = models.CharField(max_length=4)
+    summoner_icon = models.CharField(max_length=5)
+
+
+class Champion(models.Model):
+    champion_name = models.CharField(max_length=30)
+    champion_title = models.CharField(max_length=80)
+    champion_attack = models.IntegerField()
+    champion_defense = models.IntegerField()
+    champion_magic = models.IntegerField()
+    champion_difficulty = models.IntegerField()
--- leaguesearch/searchapp/views.py
+++ leaguesearch/searchapp/views.py
@@ -1,4 +1,46 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import requests
 
-# Create your views here.
+from .models import Summoner, Champion
 
+API_KEY = ''
+
+
+def champions(request):
+    try:
+        url = 'https://ddragon.leagueoflegends.com/cdn/14.1.1/data/en_US/champion.json'
+
+        r = requests.get(url)
+        champion_data = r.json()
+
+        champions = []
+
+        data = champion_data['data']
+
+        for c in data:
+            champ = data[c]
+            title = champ['title']
+            info = champ['info']
+            attack = info.get('attack')
+            defense = info.get('defense')
+            magic = info.get('magic')
+            difficulty = info.get('difficulty')
+            champion = Champion(champion_name=c, champion_title=title, champion_attack=attack, champion_defense=defense,
+                                champion_magic=magic, champion_difficulty=difficulty)
+            champions.append(champion)
+
+        context = {
+            'champions': champions
+        }
+
+        return render(request, 'champions.html', context)
+    except requests.RequestException as e:
+        print(e)
+        return HttpResponse('There has been an error retrieving champion data, please try again later')
+
+
+
+
+def index(request):
+    return render(request, 'index.html')
